c4cf60ffa6db184c51b8151c2ebe5c280df64078
MariaS111 <mariasviridova470@gmail.com>
2023-10-05 18:05:31 +0300

    feat: add api pytest

--- Library/api/tests.py
+++ Library/api/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from .serializers import BookSerializer
+from books.models import Book
+from authors.models import Author
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+def test_get_all_books(api_client):
+    author1 = Author.objects.create(full_name='Test', date_of_birth='1990-10-09', biography='Test')
+    book1 = Book.objects.create(title='Book 1', author=author1, publication_year=1)
+    url = reverse('books_api')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
