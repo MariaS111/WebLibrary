134a297bb2f66a4d5d27dc80303660f2a2ec42a9
MariaS111 <mariasviridova470@gmail.com>
2023-10-08 12:52:11 +0300

    feat: modify settings

--- Library/Library/settings.py
+++ Library/Library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+^)@icx782&ui^snyfm!bo0o)ph4!ek*-xoctlcx)(#g233aeb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
